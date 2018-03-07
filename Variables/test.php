fe33ceb204d6eda1fbd4159f65a0d62abe680b9e
PrinceKombo <princekombo@gmail.com>
2018-03-07 16:56:56 +0100

    First commit
    -Description of the exercices
    -First use of php

---
+++ Variables/test.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exo</title>
+  </head>
+  <body>
+    <h1 class="title">Variables</h1>
+    <h4 class="Exo">Exercice 1</h4>
+    <p>Create a variable name and initialize it with a value of your choice
+        <br> Show its content.</p>
+
+
+    <?php
+    // Declaring variables
+    $name = "In the begining there was blackness ";
+    $level = " the level was over ";
+    $lvlup = 90000;
+
+    // Displaying variables value
+    echo $name;  // Output: Hello World!
+    echo $level; // Output: 10
+    echo $lvlup; // Output: 90000
+    ?>
+
+    <h4 class="Exo">Exercice 2</h4>
+    <p>Create three variables name, firstname and age and initialize them with values of your choice. <br> age is an integer. <br>
+    Show its content.</p>
+
+    <br>
+
+    <?php
+    //DEclaring the variables values
+    $nom = "Bold ";
+    $prenom = " Mama Fuckaaaaz ";
+    $age = " Over 900000 ";
+
+    //Displaying the variable on the screen
+    echo $nom; //display Bold
+    echo $prenom; //display Mama Fuckaaaaz
+    echo $age; //display Over 90000
+     ?>
+
+     <h4 class="Exo">Exercice 3 </h4>
+     <p>Create a variable km. Initialize it to 1.
+       <br> Show its content. <br>
+       <?php
+
+       //Declaring the var km to 1
+       $km = 1;
+
+       //Displaying the var km on the screan
+
+       echo $km;
+        ?>
+     <br> Change its value to 3.
+     <br> Show its content. <br>
+     <?php
+
+     //Declaring the var km to 3
+     $km = 3;
+
+     //Displaying the var km on the screan
+     echo $km;
+      ?>
+     <br>  Change its value to 125.
+     <br>Show its content.</p>
+     <?php
+     //Declaring the var km to 125
+     $km = 125;
+     //Displaying the var km on the screan
+     echo $km;
+      ?>
+
+      <h4 class="Exo">Exercice 4 </h4>
+    <p>Create a variable of type string, a variable of type int, a variable of type float, a variable of type boolean and initialize them with values of your choice.
+      <br>
+    Show them.</p>
+
+    <!-- Exercice 5
+    Create a variable of type int.
+    Leave it empty.
+    Show its value.
+    Give it a value and show it. -->
+
+    <!-- Exercice 6
+    Create a variable name and initialize it with a value of your choice.
+    Show : "Hi" + name + ", how are you ?". -->
+
+    <!-- Exercice 7
+    Create three variables name, firstname and age and initialize them with values of your choice. Age is of type integer.
+    Show : "Hi" + name + firstname + ", you are" + age + "year old". -->
+
+  </body>
+</html>
